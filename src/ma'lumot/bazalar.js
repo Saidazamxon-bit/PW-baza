17fb7fec937d7832c1473311c133e8c5ad4ef7f5
Saidazamxon Orifjonov <saidazamxonx@gmail.com>
2025-10-21 14:09:24 +0300

    Update bazalar.js

--- src/ma'lumot/bazalar.js
+++ src/ma'lumot/bazalar.js
@@ -1,14 +1,26 @@
-// src/ma'lumot/bazalar.js
 export const mijozlarData = [
-  {
-    id: "Uz123456",
-    ism: "Jasur",
-    familya: "Orifov",
-    telefon: "+998901234567",
-    email: "jasur@example.com",
-    manzil: "Toshkent",
-    distibyutr: "Distribyuter A",
-    sana: "2025-10-21",
-    yatt: "1000"
-  }
-];
+    {
+      id: "1",
+      ism: "Jasur",
+      familya: "Orifov",
+      telefon: "+998901234567",
+      email: "jasur@example.com",
+      manzil: "Toshkent, Shayxontohur",
+      distibyutr: "Distribyuter A",
+      sana: "2025-10-21",
+      yatt: "1000"
+    },
+    {
+      id: "2",
+      ism: "Dilorom",
+      familya: "Xudoyberdiyeva",
+      telefon: "+998909876543",
+      email: "dilorom@example.com",
+      manzil: "Toshkent, Yunusobod",
+      distibyutr: "Distribyuter B",
+      sana: "2025-10-20",
+      yatt: "1500"
+    }
+  ];
+
+  
