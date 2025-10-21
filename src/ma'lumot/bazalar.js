af94da2f85d106beb44f14fdcb4ed970f3d644b1
Saidazamxon Orifjonov <saidazamxonx@gmail.com>
2025-10-21 14:01:28 +0300

    Update bazalar.js

---
+++ src/ma'lumot/bazalar.js
@@ -0,0 +1,14 @@
+// src/ma'lumot/bazalar.js
+export const mijozlarData = [
+  {
+    id: "Uz123456",
+    ism: "Jasur",
+    familya: "Orifov",
+    telefon: "+998901234567",
+    email: "jasur@example.com",
+    manzil: "Toshkent",
+    distibyutr: "Distribyuter A",
+    sana: "2025-10-21",
+    yatt: "1000"
+  }
+];
